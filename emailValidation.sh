db89f8db0f2242f225a850ef4ce92c7d190ff5be
Tushar-W <twankhede95@gmail.com>
2020-04-11 10:56:51 +0530

    ADDED:check first part of email address

---
+++ emailValidation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+shopt -s extglob #turn on extended globbing
+
+echo "Welcome To Email Validation"
+#PATTERN
+pattern="^[0-9a-zA-Z]"
+
+#checking email pattern valid or not
+function checkPattern() {
+	if [[ $email =~ $pattern ]];
+	then
+		echo "Email is Valid"
+	else
+		echo "Email is Invalid"
+	fi
+}
+
+#read input from user
+read -p "Enter Eamil Address:" email
+checkPattern
