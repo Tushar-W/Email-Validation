c6a3bfa9e6263eb54daa0be59eadaa48d6b2968d
Tushar-W <twankhede95@gmail.com>
2020-04-11 21:44:16 +0530

    ADDED:validate second mandatory part of email address

--- emailValidation.sh
+++ emailValidation.sh
@@ -3,7 +3,7 @@ shopt -s extglob #turn on extended globbing
 
 echo "Welcome To Email Validation"
 #PATTERN
-pattern="^[0-9a-zA-Z]"
+pattern="[@][0-9a-zA-Z]"
 
 #checking email pattern valid or not
 function checkPattern() {
