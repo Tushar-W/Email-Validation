fd02a1642aaf0db80ef719d0a666ff7c1dcdba21
Tushar-W <twankhede95@gmail.com>
2020-04-12 00:53:02 +0530

    ADDED:validate optional part of eamil address

--- emailValidation.sh
+++ emailValidation.sh
@@ -1,9 +1,10 @@
+
 #!/bin/bash -x
 shopt -s extglob #turn on extended globbing
 
 echo "Welcome To Email Validation"
 #PATTERN
-pattern="^([._+-]{1}[0-9a-zA-Z]+)?$"
+pattern="^[0-9a-zA-Z]{1,}([._+-]{1}[a-zA-Z]+)?[@]{1}[0-9a-zA-Z]{1,}[.]{1}[a-z]{2,4}([.]{1}[a-z]{2})?$"
 
 #checking email pattern valid or not
 function checkPattern() {
